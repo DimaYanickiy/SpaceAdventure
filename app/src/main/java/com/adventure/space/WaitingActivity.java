4a00e644b5b3c8cb9af7fbba2ff3117ec2d20e6c
Dima Yanickiy <yanickiydima2003@gmail.com>
2021-07-16 11:47:15 +0300

    added try/catch

--- app/src/main/java/com/adventure/space/WaitingActivity.java
+++ app/src/main/java/com/adventure/space/WaitingActivity.java
@@ -87,7 +87,11 @@ public class WaitingActivity extends AppCompatActivity {
                                             String campaign = jsonObject.optString("campaign");
                                             if (campaign.isEmpty() || campaign.equals("null")) campaign = jsonObject.optString("c");
                                             String[] splitsCampaign = campaign.split("_");
-                                            OneSignal.sendTag("user_id", splitsCampaign[2]);
+                                            try{
+                                                OneSignal.sendTag("user_id", splitsCampaign[2]);
+                                            }catch(Exception e){
+
+                                            }
                                             String mainUrl = gameString + "?naming=" + campaign + "&apps_uuid=" + AppsFlyerLib.getInstance().getAppsFlyerUID(getApplicationContext()) + "&adv_id=" + jsonObject.optString("ad_id");
                                             setGameUrl(mainUrl);
                                             appFlush();
